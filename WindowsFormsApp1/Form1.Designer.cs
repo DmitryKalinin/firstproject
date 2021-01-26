17c5ccfa151eaef9126ba3039538c2a0f46940e5
DmitryKalinin <kalinin.dmitriy.1996@gmail.com>
2021-01-26 12:44:36 +0700

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.alphabet = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.setTable = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.beginState = new System.Windows.Forms.TextBox();
+            this.chekList = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label6 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // alphabet
+            // 
+            this.alphabet.Location = new System.Drawing.Point(21, 102);
+            this.alphabet.Name = "alphabet";
+            this.alphabet.Size = new System.Drawing.Size(152, 20);
+            this.alphabet.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 142);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Алфавит";
+            // 
+            // setTable
+            // 
+            this.setTable.Location = new System.Drawing.Point(21, 260);
+            this.setTable.Name = "setTable";
+            this.setTable.Size = new System.Drawing.Size(152, 34);
+            this.setTable.TabIndex = 4;
+            this.setTable.Text = "Регулярная грамматика";
+            this.setTable.UseVisualStyleBackColor = true;
+            this.setTable.Click += new System.EventHandler(this.setTable_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 145);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(152, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Фиксированная подцепочка";
+            // 
+            // beginState
+            // 
+            this.beginState.Location = new System.Drawing.Point(21, 161);
+            this.beginState.Name = "beginState";
+            this.beginState.Size = new System.Drawing.Size(152, 20);
+            this.beginState.TabIndex = 6;
+            // 
+            // chekList
+            // 
+            this.chekList.Enabled = false;
+            this.chekList.Location = new System.Drawing.Point(546, 260);
+            this.chekList.Name = "chekList";
+            this.chekList.Size = new System.Drawing.Size(110, 37);
+            this.chekList.TabIndex = 10;
+            this.chekList.Text = "Построить цепочки";
+            this.chekList.UseVisualStyleBackColor = true;
+            this.chekList.Click += new System.EventHandler(this.chekList_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(235, 305);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(421, 202);
+            this.richTextBox1.TabIndex = 12;
+            this.richTextBox1.Text = "";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(235, 27);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(421, 215);
+            this.dataGridView1.TabIndex = 13;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(24, 83);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(86, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Алфавит языка";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(27, 46);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(34, 20);
+            this.numericUpDown1.TabIndex = 15;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(79, 46);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(31, 20);
+            this.numericUpDown2.TabIndex = 16;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(21, 222);
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown3.TabIndex = 17;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 203);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Кратность";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(686, 544);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.chekList);
+            this.Controls.Add(this.beginState);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.setTable);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.alphabet);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox alphabet;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button setTable;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox beginState;
+        private System.Windows.Forms.Button chekList;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        
+
+        public char[] terms = new char[10] {'A','B','C','D','E','F','G','H','I','J'};
+        public string[] termsValue = new string[10];
+        public int termsSize;
+        public string str,strOut, outText;  
+        public char[] s_alphabet = new char[50];
+        public char[] s_beginState = new char[50];
+        public int stInd = 0, alpInd = 0;
+        public int strLength;
+        public int s_statesListLength = 0;
+        public int s_alphabetLength = 0;
+        public int s_endStateLength = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void setTable_Click(object sender, EventArgs e)
+        {
+                richTextBox1.Text = "";
+                for (int i = 0; i < 10; i++) termsValue[i] = "";
+                s_alphabetLength = alphabet.TextLength;
+                
+                //переводим строку в массив символов
+               
+                for (int i = 0; i < alphabet.TextLength; i++)
+                {
+                    s_alphabet[i] = alphabet.Text[i];
+                }
+                for (int i = 0; i < beginState.TextLength; i++)
+                {
+                    s_beginState[i] = beginState.Text[i];
+                }
+
+
+                /////////////////////////
+                ///
+                //Узнаем количество нетерминальных элементов
+                if(Convert.ToInt32(numericUpDown3.Value) > beginState.Text.Length)
+            {
+
+                termsSize = Convert.ToInt32(numericUpDown3.Value) - beginState.Text.Length + 2;
+                if (termsSize < 3) termsSize = 3;
+            }
+                //правило для А
+                for(int i = 1; i < termsSize-1; i++)
+            {
+                termsValue[0] += terms[i].ToString();
+            }
+
+            if(Convert.ToInt32(numericUpDown2.Value) / Convert.ToInt32(numericUpDown3.Value) - 1 >1)
+                for(int i = 0; i < Convert.ToInt32(numericUpDown2.Value)/Convert.ToInt32(numericUpDown3.Value)-1; i++)
+            {
+                termsValue[0] += terms[termsSize - 1];
+            }else termsValue[0] += terms[termsSize - 1];
+
+            termsValue[0] += beginState.Text;
+            //Правило для всех остальных кроме последнего
+            for(int i = 1; i < termsSize - 1; i++)
+            {
+                for (int j = 0; j < alphabet.Text.Length; j++)
+                {
+                    termsValue[i] += alphabet.Text[j];
+                }
+            }
+            //правило для последнего нетерминала
+            for (int i = 0; i < termsSize; i++)
+            {
+                termsValue[termsSize - 1] += terms[termsSize - 2];
+            }
+            termsValue[termsSize - 1] += "|lam";
+
+            for (int i = 0; i < termsSize; i++)
+            {
+                richTextBox1.Text += termsValue[i] + '\n';
+            }
+           
+            
+            /*
+
+                richTextBox1.Text = "";
+                //задаем заблицу переходов
+                for (int i = 0; i < s_alphabetLength; i++)
+                {
+                    dataGridView1.Columns.Add("", s_alphabet[i].ToString());
+                }
+                for (int i = 0; i < s_statesListLength; i++)
+                {
+                    dataGridView1.Rows.Add();
+                }
+                for (int i = 0; (i < (dataGridView1.Rows.Count - 1)); i++)
+                {
+                   // dataGridView1.Rows[i].HeaderCell.Value = s_statesList[i].ToString();
+                }
+
+                for (int i = 0; i < s_alphabetLength; i++)
+                {
+                      for (int j = 0; j < s_statesListLength; j++)
+                       {
+                            dataGridView1.Rows[j].Cells[i].Value="0";
+                       }     
+                }
+
+                chekList.Enabled=true;
+            */
+
+        }
+        private void chekList_Click(object sender, EventArgs e)
+        {
+            string current = termsValue[0];
+            while (isterms(current) != 0)
+            {
+
+            }
+
+
+        }
+
+
+        public int isterms(string current)
+        {
+            for(int i = 0; i < terms.Length; i++)
+            {
+                for(int j = 0; j < current.Length; j++)
+                {
+                    if (current[j] == terms[i])
+                    {
+                        return i;
+                    }
+                }
+            }
+
+
+            return 0;
+        }
+
+
+        public bool stateInAlphabet(char[] a,int aL, char[] b, int bL)
+        {
+            for (int i = 0; i < aL; i++)
+            {
+                for (int j = 0; j < bL; j++)
+                {
+                    if (a[i] == b[j]) return false;
+                }
+            }
+            return true;
+        }
+        public bool charInString(char c, char[] b, int bL)
+        {
+            for (int i = 0; i < bL; i++)
+            {
+                if (b[i] == c) return true;
+            }
+            return false;
+        }
+        public bool stateInEndState(char[] a, int aL, char[] b, int bL)
+        {
+            bool flag = false;
+            for (int i = 0; i < aL; i++)
+            {
+                flag = false;
+                for (int j = 0; j < bL; j++)
+                {
+                    if (a[i] == b[j]) flag=true;
+                }
+                if (flag == false) return false;
+            }
+
+            return true;
+        }
+        public bool tableCheck(char[,] a,int a1L,int a2L, char[] b, int bL)
+        {
+            bool flag;
+                for (int i = 0; i < a1L; i++)
+                {
+                    for (int j = 0; j < a2L; j++)
+                    {
+                    flag = false;   
+                    for (int k = 0; k < bL; k++)
+                    {
+                        
+                        if (a[i,j].ToString() == b[k].ToString())
+                        {
+                            flag = true;
+                        }
+                        
+                    }
+                    if (flag == false) return false;
+                }
+                }
+            return true;
+        }
+        public bool strCheck(string s, char[] a, int aL)
+        {
+            for (int i = 0; i < s.Length; i++)
+            {
+                bool flag = false;  
+                for (int j = 0; j < aL; j++)
+                {
+                    if (str[i]==a[j])
+                    {
+                        return true;
+                    }
+                }
+                if (flag == false) return false;
+            }
+            return true;
+        }
+    }
+}
